013c9ad94c183a2d36f7a6a485f0b14caa821984
Marco Aurelio <markinho9_7@hotmail.com>
2019-03-13 13:14:10 -0300

    Testes 4 e 5

---
+++ src/testes/Teste.java
@@ -0,0 +1,37 @@
+package testes;
+
+import classes.Jogo;
+import classes.Participante;
+import classes.Resultado;
+import org.junit.*;
+
+public class Teste {
+    
+    @Test
+    public void deveTerJogoComUmUnicoParticipante() {
+        Jogo jogo = new Jogo("Basquete");
+        
+        Assert.assertEquals(0, jogo.getResultados().size());
+        
+        jogo.anota(new Resultado(new Participante("Marco"), 462));
+        
+        Assert.assertEquals(1, jogo.getResultados().size());
+        Assert.assertEquals(462, jogo.getResultados().get(0).getMetrica(), 0.00001);
+    }
+    
+    @Test
+    public void deveTerJogoComVariosParticipantes() {
+        Jogo jogo = new Jogo("Futebol");
+
+        Assert.assertEquals(0, jogo.getResultados().size());
+        
+        jogo.anota(new Resultado(new Participante("Marco"), 462));
+        jogo.anota(new Resultado(new Participante("Guilherme"), 15));
+        jogo.anota(new Resultado(new Participante("Bruno"), 44));
+        jogo.anota(new Resultado(new Participante("Vinicius"), 897));
+        
+        Assert.assertEquals(4, jogo.getResultados().size());
+        Assert.assertEquals("Bruno", jogo.getResultados().get(2).getParticipante().getNome());
+        Assert.assertEquals(15, jogo.getResultados().get(1).getMetrica(), 0.00001);
+    }
+}
